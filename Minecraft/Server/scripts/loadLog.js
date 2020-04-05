40049f9cc1ba20f88480abf5f70e0874a4231b7e
Stehsaer <54050160+Stehsaer@users.noreply.github.com>
2020-04-05 16:18:40 +0800

    0048

--- Minecraft/Server/scripts/loadLog.js
+++ Minecraft/Server/scripts/loadLog.js
@@ -1,4 +1,39 @@
 ﻿function load() {
-    var obj = $.getJSON("/log/logMap.json");
-    alert(obj.length);
+    $("body").html("");
+    var xhr = new XMLHttpRequest()
+    xhr.open("GET", "https://stehsaer.github.io/Minecraft/Server/log/logMap.json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 3 && xhr.status == 200) {
+            proceed(xhr.responseText);
+        }
+    }
+    xhr.send(null);
+}
+
+function proceed(response) {
+    var json = JSON.parse(response);
+    var str = "";
+    for (var i = 0; i < json.length; i++) {
+        str += "<div class='subContainer'><div class='date'>#" + json[i].year + "-" + json[i].month + "-" + json[i].date + "</div>";
+        for (var j = 0; j < json[i].fileCount; j++) {
+            var link = json[i].year + "-" + pad(json[i].month, 2) + "-" + pad(json[i].date, 2) + "-" + j + ".log";
+            str += "<span class='subLink' onclick=jumpTo('log" + "/" + link + "')>";
+            str += link+ "</span>";
+        }
+        str += "</div>";
+    }
+    $("body").html(str);
+}
+
+function jumpTo(src) {
+    window.location.href = src;
+}
+
+function pad(num, n) {
+    var len = num.toString().length;
+    while (len < n) {
+        num = "0" + num;
+        len++;
+    }
+    return num;
 }
