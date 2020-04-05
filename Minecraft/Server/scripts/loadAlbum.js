1e4fb2878deedff6b532398bd66a718e272ef4fe
Stehsaer <54050160+Stehsaer@users.noreply.github.com>
2020-04-05 11:43:08 +0800

    0045

--- Minecraft/Server/scripts/loadAlbum.js
+++ Minecraft/Server/scripts/loadAlbum.js
@@ -1,4 +1,6 @@
-﻿function load() {
+﻿var source;
+
+function load() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
@@ -11,7 +13,13 @@
 
 function handleRespond(dat) {
     var obj = JSON.parse(dat);
-    var str ="<div id='title'><div id='title_content'>服务器相册</div>";
+    if (window.location.href.split('=')[1] == undefined) {
+        var str = "<div id='title'><div id='title_content'>服务器相册</div>";
+    }
+    else {
+        var str = "<img id='backButton' src='../../globalImages/loading_small.gif' onclick='back()' data-original='../../globalImages/back.png' /><div id='title'><div id='title_content'>服务器相册</div>";
+        source = window.location.href.split('=')[1];
+    }
     for (var i = obj.length - 1; i >= 0; i--) {
         str += "</div><div class='dateContainer'><div class='dateIndicator'>";
         str += obj[i].year + "-"; str += obj[i].month + "-"; str += obj[i].date;
@@ -33,25 +41,33 @@ $(window).resize(function () {
     if (width < height) {
         $(".dateContainer").attr("style", "margin-left:2.5%;margin-right:2.5%;width:95%");
         $(".dateIndicator").attr("style", "font-size:70px;line-height:100px");
+        $("#backButton").hide(500);
     }
     else {
         $(".dateContainer").attr("style", "margin-left:20%;margin-right:20%;width:60%;");
         $(".dateIndicator").attr("style", "font-size:30px;line-height:40px");
+        $("#backButton").show(500);
     }
 });
 
 function reAttr() {
+    $(function () {
+        $("img").lazyload({ effect: "fadeIn", failureLimit: 10 });
+    });
     var width = this.innerWidth;
     var height = this.innerHeight;
     if (width < height) {
         $(".dateContainer").attr("style", "margin-left:2.5%;margin-right:2.5%;width:95%");
         $(".dateIndicator").attr("style", "font-size:70px;line-height:100px");
+        $("#backButton").hide(500);
     }
     else {
         $(".dateContainer").attr("style", "margin-left:20%;margin-right:20%;width:60%;");
         $(".dateIndicator").attr("style", "font-size:30px;line-height:40px");
+        $("#backButton").show(500);
     }
-    $(function () {
-        $("img").lazyload({ effect: "fadeIn", failureLimit: 10 });
-    });
+}
+
+function back() {
+    if (window.location.href.split('=')[1] != undefined) { window.location.href = source; }
 }
